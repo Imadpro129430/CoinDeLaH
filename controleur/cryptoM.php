5bf07fe6943015a2c6bfb890d32eb5ce9bd8b416
imad <you@example.com>
2024-04-18 22:22:24 +0200

    ajout dans cryptoM.php des methodes car j'avais oublier :D

--- controleur/cryptoM.php
+++ controleur/cryptoM.php
@@ -1,6 +1,6 @@
 <?php
-if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
-    $racine="..";
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine = "..";
 }
 include "$racine/modele/bd.crypto.php";
 include_once "$racine/modele/bd.photo.php";
@@ -9,7 +9,16 @@ include_once "$racine/modele/bd.photo.php";
 $cryptomonnaie = $_GET['Id_cryptomonnaie'] ?? 'btc'; // Par défaut, afficher Bitcoin (btc)
 
 
-$Crypto = getcrypto($Id_cryptomonnaie);
+$Crypto = getCryptoById($Id_cryptomonnaie);
+$LesPrix = getPrixById($Id_cryptomonnaie);
+$LesPrixMax = getPrixMaxById($Id_cryptomonnaie);
+$Lesnoms = getNomById($Id_cryptomonnaie);
+$LesAlgo = getAlgorithmeById($Id_cryptomonnaie);
+$LesDetails = getDetailById($Id_cryptomonnaie);
+$LesCapitalisations = getcapitalisationById($Id_cryptomonnaie);
+$Date_De_Creation = getDateDeCreationById($Id_cryptomonnaie);
+$LesPhotos = getPhotoById_Cryptomonnaie($Id_cryptomonnaie);
+
 // appel du script de vue qui permet de gerer l'affichage des donnees
 $titre = "Accueil - coin de la hess.fr";
 include "$racine/vue/entete.html.php";
